37b5dc0aa7e65dad96c92431a314c71c5d507076
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-04-30 23:50:19 +0530

    Improved Splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -9,9 +9,35 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         color: primaryBackgroundColor,
-        padding: const EdgeInsets.all(89),
-        child: Center(
-          child: Image.asset("assets/envisage_logo.png"),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(89),
+              child: Center(
+                child: Image.asset("assets/envisage_logo.png"),
+              ),
+            ),
+            Positioned(
+              child: Image.asset("assets/splash_screen/ellipse_top.png"),
+              right: 0,
+              top: 0,
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+            Positioned(
+              child:
+                  Image.asset("assets/splash_screen/ellipse_bottom_left.png"),
+              left: 0,
+              bottom: 0,
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+            Positioned(
+              child:
+                  Image.asset("assets/splash_screen/ellipse_bottom_right.png"),
+              right: 0,
+              bottom: 0,
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+          ],
         ),
       ),
     );
