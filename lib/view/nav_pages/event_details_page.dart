b1a6e18bf1e9ecbb8addf19d161ab2a4e4b6659c
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-05-11 11:25:05 +0530

    Build checkout for solo events

--- lib/view/nav_pages/event_details_page.dart
+++ lib/view/nav_pages/event_details_page.dart
@@ -1,7 +1,9 @@
 import 'package:envisage_app/controller/authentication/authentication_service.dart';
-import 'package:envisage_app/controller/cart/cart_controller.dart';
 import 'package:envisage_app/model/events_details.dart';
+import 'package:envisage_app/model/order.dart';
 import 'package:envisage_app/utils/colors.dart';
+import 'package:envisage_app/utils/services.dart';
+import 'package:envisage_app/view/nav_pages/events.dart';
 import 'package:envisage_app/view/nav_pages/team_details_for_2.dart';
 import 'package:envisage_app/view/nav_pages/team_details_for_4.dart';
 import 'package:envisage_app/view/screen.dart';
@@ -9,8 +11,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:iconly/iconly.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:async';
 
 class EventDetailsPage extends StatefulWidget {
   final bool isUpcoming;
@@ -28,6 +30,9 @@ class EventDetailsPage extends StatefulWidget {
 }
 
 class _EventDetailsPageState extends State<EventDetailsPage> {
+  final _razorpay = Razorpay();
+  Order? orderDetails;
+
   final bool isUpcoming;
   final int eventIndex;
 
@@ -49,17 +54,68 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
     });
   }
 
+  void getOrderData() async {
+    orderDetails = await OrderHandle().getOrder();
+  }
+
   void launchWhatsapp({@required number, @required message}) async {
     final Uri _url = Uri.parse("whatsapp://send?phone=$number&text=$message");
-    //final Uri _url  = Uri.parse("https://www.youtube.com/");
 
     if (!await launchUrl(_url)) throw 'Could not launch $_url';
   }
 
   @override
   void initState() {
-    getData();
     super.initState();
+    getData();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
+  String _handlePaymentSuccess(PaymentSuccessResponse response) {
+    print('Success Response: $response');
+    return "success";
+  }
+
+  String _handlePaymentError(PaymentFailureResponse response) {
+    // Do something when payment fails
+    print('Error Response: $response');
+    return response.toString();
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    // Do something when an external wallet is selected
+    print('External SDK Response: $response');
+  }
+// MIr4H6uK6tebUGCpIYMGIHdp
+
+  void openCheckout(double amount, String eventName) async {
+    getOrderData();
+    int value = (amount * 100).toInt();
+    var options = {
+      'key': 'rzp_test_mWdZk20UX7IlbJ',
+      'amount': value, //in the smallest currency sub-unit.
+      'name': 'IIC TMSL',
+      'order_id': orderDetails?.id, // Generate order_id using Orders API
+      'description': eventName,
+      'timeout': 600, // in seconds
+      'prefill': {
+        'email': await AuthenticationService().fetchEmail(),
+      },
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (err) {
+      debugPrint('Error: $err');
+    }
   }
 
   @override
@@ -222,6 +278,165 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
           );
   }
 
+  Future<dynamic> bottomSheet(BuildContext context, EventDetails _event) {
+    double subTotal = 0;
+    double transactionCharge = 0;
+    double total = 0;
+
+    subTotal = double.parse(_event.price.toString());
+    transactionCharge = 0.02 * subTotal;
+    total = subTotal + transactionCharge;
+
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
+
+    return showModalBottomSheet(
+        backgroundColor: primaryBackgroundColor,
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _width * 0.077,
+                  vertical: _width * 0.02,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Subtotal",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Text(
+                      "₹ $subTotal",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _width * 0.077,
+                  vertical: _width * 0.02,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Transaction Charge",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Text(
+                      "₹ $transactionCharge",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _width * 0.077,
+                  vertical: _width * 0.02,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Total",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Text(
+                      " ₹ $total",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "By clicking “Purchase”, you accept the terms & conditions.",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: _width * 0.077,
+                  ),
+                  child: CheckoutButton(
+                    _height,
+                    total,
+                    _event.name,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Material CheckoutButton(double _height, double amount, String eventName) {
+    return Material(
+      color: primaryHighlightColor,
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      child: InkWell(
+        splashColor: Colors.white30,
+        onTap: () {
+          openCheckout(amount, eventName);
+        },
+        child: Container(
+          height: _height * 0.0738,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                " PURCHASE ",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Image.asset(
+                  "assets/icons/forward_arrow_circle.png",
+                  height: 30,
+                  width: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   Material ATCButton(double _height, int _price, EventDetails _event) {
     return Material(
       color: primaryHighlightColor,
@@ -274,15 +489,21 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
         //
         //
         // Add Payment gateway here !!!!!!
+        var paymentStatus = await bottomSheet(context, _event);
         //
         //
-        String status = await AuthenticationService().registerSoloEvent(_event);
-        if (status == "success") {
-          Fluttertoast.showToast(msg: "Successfully registered for event");
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => style()));
+        if (paymentStatus == "success") {
+          String status =
+              await AuthenticationService().registerSoloEvent(_event);
+          if (status == "success") {
+            Fluttertoast.showToast(msg: "Successfully registered for event");
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => style()));
+          } else {
+            Fluttertoast.showToast(msg: status);
+          }
         } else {
-          Fluttertoast.showToast(msg: status);
+          Fluttertoast.showToast(msg: "Payment Failed");
         }
       }
     } else {
@@ -352,7 +573,7 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
                             message:
                                 "Hey there, I need some help in your event.");
                       },
-                      icon: Icon(FontAwesomeIcons.whatsapp,
+                      icon: const Icon(FontAwesomeIcons.whatsapp,
                           color: Color(0xff5669FF), size: 30),
                     ),
                   ),
