dc6d834dbccbbeee2e52d633ecb3138a0e0ea29a
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-05-21 22:02:58 +0530

    sponsor issue

--- lib/view/menu_pages/sponsors.dart
+++ lib/view/menu_pages/sponsors.dart
@@ -10,56 +10,55 @@ import 'package:scroll_to_index/scroll_to_index.dart';
 
 List<Map> sponser = [
   {
-    'name':'unstop | Formerly D2C',
+    'name': 'unstop | Formerly D2C',
     'type': 'Powered By',
     'logo': 'assets/Sponsors/unstop.png',
   },
   {
-    'name':'Bank of Baroda',
+    'name': 'Bank of Baroda',
     'type': '\n\n\nEvent Partners',
     'logo': 'assets/Sponsors/baroda.png',
   },
   {
-    'name':'Freeflow',
+    'name': 'Freeflow',
     'type': '',
     'logo': 'assets/Sponsors/freeflow.png',
   },
   {
-    'name':'Wow! China',
+    'name': 'Wow! China',
     'type': '',
     'logo': 'assets/Sponsors/wowchina.jpg',
   },
   {
-    'name':'BoookD.com',
+    'name': 'BoookD.com',
     'type': '',
     'logo': 'assets/Sponsors/boookd.png',
   },
   {
-    'name':'Voxit Media Tech',
+    'name': 'Voxit Media Tech',
     'type': '',
-    'logo': 'assets/Sponsors/baroda.png',
+    'logo': 'assets/Sponsors/1x1.png',
   },
   {
-    'name':'Solvoix',
+    'name': 'Solvoix',
     'type': '\n\n\nEvent Partner \nfor Biz-Plan Event',
     'logo': 'assets/Sponsors/solvoix.png',
   },
   {
-    'name':'Exchange 22',
+    'name': 'Exchange 22',
     'type': '\n\n\nEvent Partner \nfor Mock IPL Event',
     'logo': 'assets/Sponsors/exchange22.png',
   },
   {
-    'name':'Dalal Street \nInvestment Journal',
+    'name': 'Dalal Street \nInvestment Journal',
     'type': '\n\n\nEvent Partner \nfor Stockify Event',
     'logo': 'assets/Sponsors/dalalstreet.png',
   },
   {
-    'name':'Learning While Travelling',
+    'name': 'Learning While Travelling',
     'type': '\n\n\nKnowledge Partner',
     'logo': 'assets/Sponsors/lwt.png',
   },
-
   {
     'name': "Co Founder's Planet",
     'type': '\n\nVenture Partner',
@@ -73,11 +72,9 @@ List<Map> sponser = [
   {
     'name': "Creatale Fashions & Accessories",
     'type': '\n\nMerchandise Partner',
-    'logo': 'assets/Sponsors/cofounder.png',
+    'logo': 'assets/Sponsors/1x1.png',
   },
-
 ];
-
 
 class Sponser extends StatelessWidget {
   const Sponser({Key? key}) : super(key: key);
@@ -85,126 +82,203 @@ class Sponser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryBackgroundColor,
-      appBar: PreferredSize(
-        child: SafeArea(
-            child: Container(
-              // child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Get.width * 0.03,vertical: Get.height*0.01,),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(IconlyLight.arrow_left,
-                            color: Colors.white),
-                        onPressed: () {
-                          Get.back();
-                        },
-                      ),
-                      Text(
-                        "Sponsors",
-                        style: TextStyle(
-                          color: Colors.white,
-                          letterSpacing: 3,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
+        backgroundColor: primaryBackgroundColor,
+        appBar: PreferredSize(
+          child: SafeArea(
+              child: Container(
+                  // child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Get.width * 0.03,
+                    vertical: Get.height * 0.01,
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon:
+                              Icon(IconlyLight.arrow_left, color: Colors.white),
+                          onPressed: () {
+                            Get.back();
+                          },
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {Get.to(() => NotificationPage());},
-                        icon: Icon(
-                          IconlyLight.notification,
-                          color: Colors.white,
+                        Text(
+                          "Sponsors",
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 3,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      ),
-                    ]))),
-        preferredSize: Size.fromHeight(Get.height * 0.1),
-      ),
-      body: Container(
-        child: ListView.builder(
-            itemBuilder: (context, index) => SponsorCard(index),
+                        IconButton(
+                          onPressed: () {
+                            Get.to(() => NotificationPage());
+                          },
+                          icon: Icon(
+                            IconlyLight.notification,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ]))),
+          preferredSize: Size.fromHeight(Get.height * 0.1),
+        ),
+        body: Container(
+            child: ListView.builder(
+          itemBuilder: (context, index) => SponsorCard(index),
           itemCount: sponser.length,
-        )
-      ),
+        )),
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
             color: grey,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
             boxShadow: const [
               BoxShadow(
                   color: Colors.deepPurpleAccent,
                   blurRadius: 20,
                   spreadRadius: 10,
-                  offset: Offset(0, 0)
-              ),
+                  offset: Offset(0, 0)),
             ],
           ),
-          padding: EdgeInsets.symmetric(vertical: 20,horizontal:10),
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           child: ScrollLoopAutoScroll(
             child: Row(
               children: [
-                Image.asset(sponser[0]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[1]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[2]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[3]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[4]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[5]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[6]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[7]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[8]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[9]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[10]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[11]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
-                Image.asset(sponser[12]["logo"], height: Get.height*0.1,),
-                SizedBox(width: 40,),
+                Image.asset(
+                  sponser[0]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[1]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[2]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[3]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[4]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[5]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[6]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[7]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[8]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[9]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[10]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[11]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Image.asset(
+                  sponser[12]["logo"],
+                  height: Get.height * 0.1,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
               ],
             ),
             delay: Duration(seconds: 0),
             duration: Duration(seconds: 60),
             scrollDirection: Axis.horizontal,
           ),
-        )
-    );
+        ));
   }
-  Widget SponsorCard(index){
+
+  Widget SponsorCard(index) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: Get.height*0.03),
-      width: Get.width*0.8,
-      child:Column(
-        children: [
+        padding:
+            EdgeInsets.symmetric(horizontal: 20, vertical: Get.height * 0.03),
+        width: Get.width * 0.8,
+        child: Column(children: [
           Center(
-              child: Text(sponser[index]["type"], style: TextStyle(
+            child: Text(
+              sponser[index]["type"],
+              style: TextStyle(
                 color: Colors.white,
                 letterSpacing: 2,
                 fontSize: 20,
                 fontWeight: FontWeight.w600,
-              ),textAlign: TextAlign.center,),
+              ),
+              textAlign: TextAlign.center,
+            ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 30),
-            child: Image.asset(sponser[index]["logo"], width: Get.width*0.5,),
+            child: Image.asset(
+              sponser[index]["logo"],
+              width: Get.width * 0.5,
+            ),
           ),
-          Text(sponser[index]["name"], style: TextStyle(
-            color: Colors.white,
-            letterSpacing: 2,
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-          ),textAlign: TextAlign.center,)
-        ]
-      )
-    );
+          Text(
+            sponser[index]["name"],
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ]));
   }
 }
